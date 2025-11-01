4aa2d32f2e3a7a9b19942464ae47f2e89886979f
Tsuyoshi Akiyama <ka090824@gmail.com>
2025-11-01 03:07:25 +0000

    Add localization cofig for real robott

--- localization_server/launch/localization.launch.py
+++ localization_server/launch/localization.launch.py
@@ -1,48 +1,73 @@
 import os
-
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    
-    nav2_yaml = os.path.join(get_package_share_directory('localization_server'), 'config', 'amcl_config_sim.yaml')
-    map_file = os.path.join(get_package_share_directory('map_server'), 'config', 'warehouse_map_sim.yaml')
-    rviz_file = os.path.join(get_package_share_directory('localization_server'), 'rviz', 'localization.rviz')
 
-    return LaunchDescription([
-        Node(
+    def launch_setup(context, *args, **kwargs):
+        map_file = context.launch_configurations['map_file']
+        map_path = os.path.join(get_package_share_directory('map_server'), 'config', map_file)
+        is_sim = 'sim' in os.path.basename(map_file).lower()
+
+        pkg_localization = get_package_share_directory('localization_server')
+        amcl_config_file = 'amcl_config_sim.yaml' if is_sim else 'amcl_config_real.yaml'
+        amcl_config = os.path.join(pkg_localization, 'config', amcl_config_file)
+        rviz_file = os.path.join(pkg_localization, 'rviz', 'localization.rviz')
+
+        print(f"[Localization Launch] map_path = {map_path}")
+        print(f"[Localization Launch] use_sim_time = {is_sim}")
+        print(f"[Localization Launch] amcl_config_file = {amcl_config_file}")
+
+        map_server_node = Node(
             package='nav2_map_server',
             executable='map_server',
             name='map_server',
             output='screen',
-            parameters=[{'use_sim_time': True}, 
-                        {'yaml_filename':map_file}]
-        ),
-            
-        Node(
+            parameters=[
+                {'use_sim_time': is_sim},
+                {'yaml_filename': map_path}
+            ]
+        )
+
+        amcl_node = Node(
             package='nav2_amcl',
             executable='amcl',
             name='amcl',
             output='screen',
-            parameters=[nav2_yaml]
-        ),
+            parameters=[amcl_config]
+        )
 
-        Node(
+        lifecycle_manager_node = Node(
             package='nav2_lifecycle_manager',
             executable='lifecycle_manager',
             name='lifecycle_manager_localization',
             output='screen',
-            parameters=[{'use_sim_time': True},
-                        {'autostart': True},
-                        {'node_names': ['map_server', 'amcl']}]
-        ),
-        Node(
+            parameters=[
+                {'use_sim_time': is_sim},
+                {'autostart': True},
+                {'node_names': ['map_server', 'amcl']}
+            ]
+        )
+
+        rviz_node = Node(
             package='rviz2',
             executable='rviz2',
             name='rviz2',
             arguments=['-d', rviz_file],
-            parameters=[{'use_sim_time': True}],
+            parameters=[{'use_sim_time': is_sim}],
             output='screen'
+        )
+
+        return [map_server_node, amcl_node, lifecycle_manager_node, rviz_node]
+
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'map_file',
+            default_value='warehouse_map_sim.yaml',
+            description='Map yaml file name'
         ),
+        OpaqueFunction(function=launch_setup)
     ])
